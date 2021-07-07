644861aac826291ee1c131192c0e940d83d3fa28
skoech <sharonkoech5147@gmail.com>
2021-07-07 08:48:57 +0300

    Tremor Test Website- VuePress

---
+++ src/.vuepress/config.js
@@ -0,0 +1,82 @@
+const { description } = require('../../package')
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'Tremor',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: 'Docs',
+        link: '/https://docs.tremor.rs//',
+      },
+      {
+        text: 'RFCs',
+        link: '/https://rfcs.tremor.rs//'
+      },
+      {
+        text: 'Blog',
+        link: '/https://www.tremor.rs/blog//'
+      },
+      {
+	text: 'Governance',
+	link: '/https://www.tremor.rs/governance//'
+      },
+      {
+	text: 'FAQs',
+	link: '/https://www.tremor.rs/faq//'
+      },
+      {
+	text: 'Community',
+	link: '/https://www.tremor.rs/community//'
+      },    
+    ],
+    sidebar: {
+      '/guide/': [
+        {
+          title: 'Guide',
+          collapsable: false,
+          children: [
+            '',
+            'using-vue',
+          ]
+        }
+      ],
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
---
+++ src/index.md
@@ -0,0 +1,15 @@
+---
+home: true
+heroImage: https://avatars.githubusercontent.com/u/60009416?s=200&v=4
+tagline: An early-stage event processing system for unstructured data with rich support for structural pattern matching, filtering and transformation.
+actionText: Getting Started
+actionLink: https://www.tremor.rs/getting-started/
+features:
+- title: Connectivity
+  details: Talking to other systems- connecting different systems is an integral part of Tremor.
+- title: Specialised Tremor Pipelines
+  details: Operators allow for highly custom behaviour.
+- title: Good User Experience
+  details: Tooling is a first-class concern for Tremor.
+footer: Made by Sharon Koech with ❤️
+---
